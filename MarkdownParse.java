34de593497af1c2145a8c0816b51f399f333450b
johnsonli626 <Junpengli0801@gmail.com>
2022-01-20 16:35:48 -0800

    new change

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,20 +10,21 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then take up to
         // the next )
         int currentIndex = 0;
-        if(markdown.indexOf("(")!=-1)
-        {
-            while(currentIndex < markdown.length()) {
+        if(markdown.indexOf("(")!=-1 && markdown.indexOf("[")!=-1)
+            {
+             while(currentIndex < markdown.length()) {
                 int nextOpenBracket = markdown.indexOf("[", currentIndex);
                 int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
                 int openParen = markdown.indexOf("(", nextCloseBracket);
                 int closeParen = markdown.indexOf(")", openParen);
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                System.out.println(currentIndex);
+                if(openParen-nextCloseBracket==1)
+                {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                }
                 currentIndex = closeParen + 1;
             
             }
         }
-        System.out.println(currentIndex);
         return toReturn;
     }
     public static void main(String[] args) throws IOException {
